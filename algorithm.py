6090314c09da37425a7ef3e663a7ca570ec1fe00
Mahdi Fooladgar <fooladgar@payampardaz.local>
2021-11-14 19:26:24 +0330

    Fix: Add TODO for example code

--- algorithm.py
+++ algorithm.py
@@ -14,4 +14,4 @@ def process_msg(src, msg):
         got_hello_from.append(src)
 
     if set(got_hello_from) == set(world.neighbors):
-        world.send_message(to=world.current_node, msg='exit')
+        world.send_message(to=world.current_node, msg='exit')  # TODO Maybe you want start your algorithm here!
